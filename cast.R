4989124cd1528f32497b5e8bb3fb2ac5fea6acc3
demandingatomiser <awkc1996@gmail.com>
2017-04-06 23:21:17 -0400

    Update cast.R

--- cast.R
+++ cast.R
@@ -1,83 +1,111 @@
+library(RNetCDF)
+library(data.table)
 library(tidyverse)
-library(curl)
-library(ggplot2)
-library(seasonal)
-library(tframe)
 library(forecast)
+library(tframe)
+library(fpp)
 
-#Get Mauna loa data
-home <- getwd()
-temp <- tempfile()
-url <- "ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt"
-curl_download(url,temp)
-data <- readLines(temp)
-unlink(temp)
+setwd("C:/Users/andrewkc/Documents/forecast")
+
+########################################################
+
+co2 <- readLines("co2_mm_mlo.txt")
 
 #Remove comment lines 
-data = data[-(1:72)]
+co2 = co2[-(1:72)]
 
 #Covert to df
-data <- data.frame(data)
+co2 <- data.frame(co2)
 
 #Parse to columns
-data <- separate(data, col="data", sep="[ ]+", into=c("year", 
+co2 <- separate(co2, col="co2", sep="[ ]+", into=c("year", 
                                                       "month", 
                                                       "decimal date", 
                                                       "average", 
                                                       "interpolated", 
                                                       "trend (season corr)", 
                                                       "days"))
+
 #Convert to numeric
-data <- as.data.frame(sapply(data, as.numeric))
+co2 <- as.data.frame(sapply(co2, as.numeric))
 
 #Add NA values
-data$average[(data$average)==-99.99] <- NA
+co2$average[(co2$average)==-99.99] <- NA
 
 #Create ts
-co2 <- ts(data$interpolated, frequency=12, start=c(1958,3))
 
-#Create ts of April levels
-April <- subset((co2), cycle(co2)==4)
+co2.cont <- ts(co2$average[(max(which(is.na(co2$average)))+1):length(co2$average)], 
+               frequency=12,  
+               start=c(1984,5))
 
-#Do initial forecast
-forecast(co2)
+co2.orig <- ts(co2$interpolated, frequency=12, start=c(1958,3))
 
-#Check default forecast method
-result <- data.frame()
-for (y in 1990:2016){
-  zz <- tfwindow(co2, end=c(y,2))
-  temp1 <- forecast(zz, h=2)
-  temp2 <- as.data.frame(temp1)[2,]
-  result <- rbind(result, temp2)
-}
+##########################################
+Wind <- read.csv("wind.csv", header=TRUE, stringsAsFactors = FALSE)
 
-#Add previous values
-result$Actual <- tail(April,dim(result)[1])
+dates <- data.frame(Time=unique(Wind$Time), Num=1:710)
 
-#Create difference to check hindcast
-result$diff <- abs(result$Actual - result$`Point Forecast`)
+Wind <- merge(Wind, dates, by="Time")
 
-#Summarize result
-summary(result$diff)
+Wind <- Wind %>%
+  group_by(Num) %>%
+  summarise(merid=mean(meridional.wind), zon=mean(zonal.wind), speed=mean(win.speed))
 
-#Check X-13SEATS method
-result <- data.frame()
-for (y in 1990:2016){
-  zz <- tfwindow(co2, end=c(y,2))
-  fit <- seas(
-    x = zz,
-    regression.aictest = NULL,
-    regression.variables = "const", 
-    forecast.save = "fct"
-  )
-  temp2 <- as.data.frame(fit$series$fct)[2,]
-  result <- rbind(result, temp2)
-}
+merid <- ts(head(Wind$merid,709), start=c(1958,3), frequency=12)
+zon <- ts(head(Wind$zon,709), start=c(1958,3), frequency=12)
+speed <- ts(head(Wind$speed,709), start=c(1958,3), frequency=12)
 
-result$Actual <- tail(April,dim(result)[1])
+########################################################
+HadSST <- read.table("HadSST.3.1.1.0.time_series.txt")
 
-result$diff <- abs(result$Actual - result$forecast)
+revtrunc <- function(x) { x - floor(x) } 
 
-summary(result$diff)
+decimals <- sort(unique(revtrunc(time(co2))))
 
+decimaldates <- data.frame("decimal" = decimals, "V2" = seq(1:12))
 
+HadSST <- HadSST[,c(1,2,12)]
+
+HadSST <- HadSST %>%
+  group_by(V1,V2) %>%
+  summarise(mean=mean(V8)) 
+
+HadSST <- ts(HadSST$mean, frequency=12, end=c(2017,3))
+
+########################################################
+
+#Make lags of wind data
+speed.lag <- tail(lag(speed),708)
+zon.lag <- tail(lag(zon),708)
+merid.lag <- tail(lag(merid),708)
+
+#Make difference of co2 data
+co2 <- tfwindow(diff(co2.orig), start=tfstart(speed.lag), end=tfend(speed.lag))
+
+#Make lag of sea surface temp data
+sst.lag <- tfwindow(lag(HadSST), start=tfstart(co2), end=tfend(co2))
+
+#Test a simple regression
+linfit <- lm(co2 ~ sst.lag + speed.lag + zon.lag + merid.lag)
+sum(coef(linfit) * c(1, tail(HadSST,1), tail(speed, 1), tail(zon, 1), tail(merid, 1)))
+
+#Form matrix of wind and sea temps
+covariates <- matrix(cbind(sst.lag,speed.lag,zon.lag,merid.lag), ncol=4, nrow=708)
+
+#Add auto arima
+fit <- auto.arima(co2, xreg=covariates)
+
+#Display residuals
+tsdisplay(arima.errors(fit), main="ARIMA errors")
+residuals.Arima(fit, type="regression")
+
+#Test residuals
+Box.test(residuals(fit),fitdf=5,lag=10,type="Ljung")
+
+#Forecast April 2017!!
+final <- forecast(fit, 
+                  xreg=cbind(tail(HadSST,1), tail(speed, 1), tail(zon, 1), tail(merid, 1)), 
+                  h=1,
+                  level=c(95,99))
+
+tail(co2.orig,1) + final$mean[1]
